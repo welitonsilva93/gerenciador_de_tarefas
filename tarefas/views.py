bfbc3ce356a93c462ad5a975c95a7d7f50e6a123
Weliton Silva <welitonsylva.2000@gmail.com>
2025-06-25 01:21:27 -0300

    pequena correção de importações

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render, redirect, get_object_or_404
 from .forms import FormularioTarefa
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse_lazy
 from . models import Tarefas, Categoria
 from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.mixins import LoginRequiredMixin
